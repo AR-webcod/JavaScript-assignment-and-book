2545072fea6ca7d30cdcd5d72a4ec4def046147f
ABDUL REHMAN <abdulrehmanars1996@gmail.com>
2026-04-22 20:22:58 +0500

    updated
    updated code

---
+++ comparison operator/app.js
@@ -0,0 +1,66 @@
+//  // Comparison Operators in JavaScript
+
+//  // Equal to (==) doble equal to ka matlab hai ke dono barabar hu tu true hu ga warna false
+
+// // let n=10
+// // let m=20
+// // console.log(n==m); 
+
+// // let a=10
+// // let b=10
+// // console.log(a==b);
+
+// // double equal to types check nahi karta hai value check karta hai 
+// // console.log(a=="10");
+
+
+//  // Equal to (===) tripple equal to types or value bhe check karta hai.
+// // let x=20
+// // let y=30
+// // console.log(x===y);    // false
+
+// // console.log("20"===y);  // fasle
+
+
+// // let c=4
+// // let d=4
+// // console.log(c===d);  // true
+
+
+
+// // Not Equal to (!=)
+
+// // let c=4
+// // let d=5
+// //  console.log(c!=d);  // true
+
+
+// let c=4
+// let d=4
+// // console.log(c!=d);   //false
+
+// // Not double Equal to (!==) types and value check
+
+// console.log(c!==d);        //false
+// console.log(c!=="4");        //true
+
+
+// Greator than ">"  left wali value bari hune chaye right se 
+
+// let c=54
+// let d=40
+// console.log(c>d);    // true
+// console.log(c<d);    // false
+
+
+// Greator than or equal "> ="  agar first divison me 60 hai true show karuan ga or 60 se bara ai tu bhe true du ga.
+let c=60
+let d=60
+console.log(c>=d);    // true
+console.log(c>=80);    // false
+ 
+
+
+
+
+
